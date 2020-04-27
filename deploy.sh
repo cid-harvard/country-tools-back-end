7569274902e2784fe9a7b1f92b610163c59e3ed2
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-04-27 13:09:48 -0400

    Spaces to tabs in script

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,17 @@ scp ./requirements.txt $SERVER_USER@$SERVER_ADDRESS:$APP_DIR/
 
 ssh $SERVER_USER@$SERVER_ADDRESS <<- EOF
 	sudo apt update
-    sudo apt install -y python3.7
-    sudo apt install -y python3-venv
-    cd $APP_DIR
-    python3 -m venv env
-    . ./env/bin/activate
-    pip install -r requirements.txt
-    export COUNTRY_TOOLS_DB_USER=$COUNTRY_TOOLS_DB_USER
-    export COUNTRY_TOOLS_DB_PASS=$COUNTRY_TOOLS_DB_PASS
-    export COUNTRY_TOOLS_DB_HOST=$COUNTRY_TOOLS_DB_HOST
-    export COUNTRY_TOOLS_DB_NAME=$COUNTRY_TOOLS_DB_NAME
-    sudo cp $APP_DIR/api/gunicorn/api.service /etc/systemd/system
-    sudo cp $APP_DIR/api/gunicorn/api.ini /etc/systemd/system
-    sudo systemctl reload api
+	sudo apt install -y python3.7
+	sudo apt install -y python3-venv
+	cd $APP_DIR
+	python3 -m venv env
+	. ./env/bin/activate
+	pip install -r requirements.txt
+	export COUNTRY_TOOLS_DB_USER=$COUNTRY_TOOLS_DB_USER
+	export COUNTRY_TOOLS_DB_PASS=$COUNTRY_TOOLS_DB_PASS
+	export COUNTRY_TOOLS_DB_HOST=$COUNTRY_TOOLS_DB_HOST
+	export COUNTRY_TOOLS_DB_NAME=$COUNTRY_TOOLS_DB_NAME
+	sudo cp $APP_DIR/api/gunicorn/api.service /etc/systemd/system
+	sudo cp $APP_DIR/api/gunicorn/api.ini /etc/systemd/system
+	sudo systemctl reload api
 EOF
