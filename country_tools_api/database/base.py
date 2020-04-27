e4bb8e1c4b4e3f5f0e0d3e5e20fc0f6f260a91ad
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-04-27 12:55:49 -0400

    Move db specs to secret env config

--- country_tools_api/database/base.py
+++ country_tools_api/database/base.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 import os
 
 # Create database engine
-db_user = "postgres"
-db_pass = "postgres"
-db_host = "country-tools-staging.cppjqzftxysd.us-east-1.rds.amazonaws.com"
-db_name = "country_tools"
+db_user = os.environ.get("COUNTRY_TOOLS_DB_USER")
+db_pass = os.environ.get("COUNTRY_TOOLS_DB_PASS")
+db_host = os.environ.get("COUNTRY_TOOLS_DB_HOST")
+db_name = os.environ.get("COUNTRY_TOOLS_DB_NAME")
 db_uri = f"postgresql://{db_user}:{db_pass}@{db_host}/{db_name}"
 engine = create_engine(db_uri, convert_unicode=True)
 
