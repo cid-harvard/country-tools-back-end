ec54dc87092a086da35f501ddec02f294e1cfa1b
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-04-27 12:50:05 -0400

    better handle branches and travis script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -xe
 
+SERVER_ADDRESS=$1
+
 # Copy Travis build
-scp ./requirements.txt $SERVER_USER@$SERVER_ADDRESS:$APP_DIR/api
 rsync -rq --delete --rsync-path="mkdir -p $APP_DIR/api && rsync" $TRAVIS_BUILD_DIR/country_tools_api $SERVER_USER@$SERVER_ADDRESS:$APP_DIR/api
+scp ./requirements.txt $SERVER_USER@$SERVER_ADDRESS:$APP_DIR/
 
 ssh $SERVER_USER@$SERVER_ADDRESS <<- EOF
 	sudo apt update
     sudo apt install -y python3.7
     sudo apt install -y python3-venv
-    cd $APP_DIR/api
+    cd $APP_DIR
     python3 -m venv env
     . ./env/bin/activate
     pip install -r requirements.txt
-    python flask_app.py
+    sudo cp $APP_DIR/api/gunicorn/api.service /etc/systemd/system
+    sudo cp $APP_DIR/api/gunicorn/api.ini /etc/systemd/system
+    sudo systemctl reload api
 EOF
