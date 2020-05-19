f35c694f5bf53917ae5a072292cb03068139145a
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-05-19 16:14:38 -0400

    Add Jordan to API

---
+++ jordan/data_copy.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from sqlalchemy import MetaData, event
+from sqlalchemy.schema import CreateSchema
+from pandas_to_postgres import DataFrameCopy
+from country_tools_api.database.base import engine, Base
+from country_tools_api.database.jordan import (
+    JordanNationality,
+    JordanControl,
+    JordanText,
+    JordanOccupation,
+    JordanSchooling,
+    JordanWageHistogram,
+    JordanMapLocation,
+    JordanGlobalTopFDI,
+    JordanRegionTopFDI,
+    JordanFactors,
+    JordanIndustry,
+)
+
+
+JOR_SCHEMA = "jordan"
+JOR_PROCESSED_DATA_DIR = "./jordan/processed_data"
+JOR_TABLES = [
+    "control",
+    "factors",
+    "global_top_fdi",
+    "industry",
+    "map_location",
+    "nationality",
+    "occupation",
+    "region_top_fdi",
+    "schooling",
+    "text",
+    "wage_histogram",
+]
+
+
+if __name__ == "__main__":
+    engine.execute(f"CREATE SCHEMA IF NOT EXISTS {JOR_SCHEMA};")
+    Base.metadata.create_all()
+
+    with engine.connect() as c:
+        meta = MetaData(bind=c, reflect=True, schema=JOR_SCHEMA)
+
+        for table in JOR_TABLES:
+            DataFrameCopy(
+                pd.read_csv(f"{JOR_PROCESSED_DATA_DIR}/{table}.csv"),
+                conn=c,
+                table_obj=meta.tables[f"{JOR_SCHEMA}.{table}"],
+            ).copy()
---
+++ jordan/ingest.py
@@ -0,0 +1,201 @@
+import pandas as pd
+from itertools import product
+
+RAW_DATA_DIR = "./raw_data"
+PROCESSED_DATA_DIR = "./processed_data"
+
+# "jordanmap.json"
+
+
+if __name__ == "__main__":
+    ### INDUSTRY -----------------------------------------------------------------------
+    industry = pd.read_csv(f"{RAW_DATA_DIR}/industry.csv")
+    industry.columns = (
+        "industry_code",
+        "title",
+        "theme",
+        "subtheme",
+        "description",
+        "keywords",
+    )
+    industry.to_csv(f"{PROCESSED_DATA_DIR}/industry.csv", index=False)
+
+    ### NATIONALITY --------------------------------------------------------------------
+    nationality = pd.DataFrame(
+        product(
+            list(industry.industry_code),
+            ["Jordanian", "Egyptian", "Syrian", "Other Arab", "Non Arab"],
+        ),
+        columns=("industry_code", "nationality"),
+    )
+    nationality_sheets = ["demographics", "wage"]
+
+    for sheet in nationality_sheets:
+        df = pd.read_csv(f"{RAW_DATA_DIR}/{sheet}.csv")
+        df.columns = [x.lower() for x in df.columns]
+        nationality = nationality.merge(
+            df,
+            how="left",
+            left_on=["industry_code", "nationality"],
+            right_on=["industry", "nationality"],
+        )
+        nationality = nationality.drop(columns=["industry"])
+    nationality.to_csv(f"{PROCESSED_DATA_DIR}/nationality.csv", index=False)
+
+    ### CONTROL ------------------------------------------------------------------------
+    control_sheets = ["laborControl", "womenControl", "fdiControl"]
+    control = industry[["industry_code"]]
+
+    for sheet in control_sheets:
+        df = pd.read_csv(f"{RAW_DATA_DIR}/{sheet}.csv", index_col="industry")[
+            ["control"]
+        ]
+        df.columns = (sheet.split("Control")[0],)
+        control = control.merge(
+            df, how="left", left_on="industry_code", right_index=True
+        )
+    control.to_csv(f"{PROCESSED_DATA_DIR}/control.csv", index=False)
+
+    ### TEXT ---------------------------------------------------------------------------
+    text = industry[["industry_code"]]
+    text_sheets = {
+        "occupationText": ("occupation",),
+        "otherText": ("demographic", "location", "avg_wage"),
+        "wagehistogramText": ("wage_hist",),
+        "scatterText": ("scatter",),
+        "schoolingText": ("schooling",),
+        # womentext combines female participation and high-skill jobs
+        "womentext": ("percent_female", "percent_high_skill", "female", "high_skill"),
+    }
+
+    for sheet, cols in text_sheets.items():
+        df = pd.read_csv(f"{RAW_DATA_DIR}/{sheet}.csv", index_col="industry").drop(
+            columns=["sic4_description", "text1"], errors="ignore"
+        )
+        df.columns = cols
+        text = text.merge(df, how="left", left_on="industry_code", right_index=True)
+    text.to_csv(f"{PROCESSED_DATA_DIR}/text.csv", index=False)
+
+    ### TOP FDI ------------------------------------------------------------------------
+    top_fdi_sheets = {
+        "FDItop10global": "global_top_fdi",
+        "FDItop10region": "region_top_fdi",
+    }
+    for sheet, new_sheet in top_fdi_sheets.items():
+        df = pd.read_csv(f"{RAW_DATA_DIR}/{sheet}.csv").drop(
+            columns=["sic4_description", "total_company_capinv", "industries119"],
+            errors="ignore",
+        )
+        df.columns = (
+            "industry_code",
+            "company",
+            "source_country",
+            "capital_investment",
+            "rank",
+        )
+        df.to_csv(f"{PROCESSED_DATA_DIR}/{new_sheet}.csv", index=False)
+
+    ### FACTORS ------------------------------------------------------------------------
+    factors = industry[["industry_code"]]
+    factor_sheets = {
+        "spider": {
+            "cols": (
+                "industry_code",
+                "rca_jordan",
+                "rca_peers",
+                "water_intensity",
+                "electricity_intensity",
+                "availability_inputs",
+                "female_employment",
+                "high_skill_employment",
+                "fdi_world",
+                "fdi_region",
+                "export_propensity",
+            )
+        },
+        "scatter": {
+            "drop": ["Theme", "Subtheme", "Description"],
+            "cols": (
+                "industry_code",
+                "viability",
+                "attractiveness",
+                "viability_median",
+                "attractiveness_median",
+                "category",
+                "rca",
+            ),
+        },
+    }
+    for sheet, config in factor_sheets.items():
+        df = pd.read_csv(f"{RAW_DATA_DIR}/{sheet}.csv").drop(
+            columns=config.get("drop", []), errors="ignore"
+        )
+        df.columns = config["cols"]
+        factors = factors.merge(df, how="left", on="industry_code")
+    factors.to_csv(f"{PROCESSED_DATA_DIR}/factors.csv", index=False)
+
+    ### OVER TIME ----------------------------------------------------------------------
+
+    over_time = pd.DataFrame(
+        columns=[
+            "industry_code",
+            "visualization",
+            "variable",
+            "years_2004_2006",
+            "years_2007_2009",
+            "years_2010_2012",
+            "years_2013_2015",
+            "years_2016_2018",
+        ]
+    )
+
+    over_time_sheets = {"fdiBarChart": "fdi_bar_chart", "histogram": "histogram"}
+
+    for sheet, viz_title in over_time_sheets.items():
+        df = (
+            pd.read_csv(f"{RAW_DATA_DIR}/{sheet}.csv")
+            .drop(
+                columns=["sic4_description", "IndustrySector", "SubSector"],
+                errors="ignore",
+            )
+            .rename(columns={"industry": "industry_code", "Variable": "variable"})
+        )
+        df["visualization"] = viz_title
+        over_time = over_time.append(df)
+
+    ### COPY OTHER SHEETS --------------------------------------------------------------
+    copy_sheets = {
+        "mapLocation": {
+            "columns": (
+                "industry_code",
+                "gov_code",
+                "governorate",
+                "share_state",
+                "share_country",
+            ),
+            "filename": "map_location",
+        },
+        "occupation": {"columns": ("industry_code", "occupation", "men", "women")},
+        "schooling": {"columns": ("industry_code", "schooling", "men", "women")},
+        "wagehistogram": {
+            ## TODO: Duped Country row for every industry
+            "columns": (
+                "industry_code",
+                "facet",
+                "range_0_100",
+                "range_100_200",
+                "range_200_300",
+                "range_300_400",
+                "range_400_500",
+                "range_500_600",
+                "range_600_plus",
+            ),
+            "filename": "wage_histogram",
+        },
+    }
+
+    for in_file, config in copy_sheets.items():
+        df = pd.read_csv(f"{RAW_DATA_DIR}/{in_file}.csv")
+        df.columns = config["columns"]
+        out_file = config.get("filename", in_file)
+        df.to_csv(f"{PROCESSED_DATA_DIR}/{out_file}.csv", index=False)
