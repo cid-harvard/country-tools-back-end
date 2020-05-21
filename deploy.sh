a9dbd426b68d80367e570a42875b590b179c4b1a
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-05-21 12:33:29 -0400

    Move database config file template and script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xe
 
+# Generate file with database config
+eval "echo \"$(cat $TRAVIS_BUILD_DIR/country_tools_api/database.tpl)\" > $TRAVIS_BUILD_DIR/country_tools_api/database"
+
 # Copy Travis build
 rsync -rq --delete --rsync-path="mkdir -p $APP_DIR/api && rsync" $TRAVIS_BUILD_DIR/country_tools_api/ $WEBSERVER_USER@$WEBSERVER_ADDRESS:$APP_DIR/api
 scp ./requirements.txt $WEBSERVER_USER@$WEBSERVER_ADDRESS:$APP_DIR/
@@ -13,11 +16,6 @@ ssh $WEBSERVER_USER@$WEBSERVER_ADDRESS <<- EOF
     python3 -m venv env
     . ./env/bin/activate
     pip install -r requirements.txt
-    export COUNTRY_TOOLS_DB_USER=$COUNTRY_TOOLS_DB_USER
-    export COUNTRY_TOOLS_DB_PASS=$COUNTRY_TOOLS_DB_PASS
-    export COUNTRY_TOOLS_DB_HOST=$COUNTRY_TOOLS_DB_HOST
-    export COUNTRY_TOOLS_DB_NAME=$COUNTRY_TOOLS_DB_NAME
-    eval "echo \"$(cat .database.tpl)\" > .database"
     sudo cp $APP_DIR/api/gunicorn/api.service /etc/systemd/system
     sudo cp $APP_DIR/api/gunicorn/api.ini /etc/systemd/system
     sudo systemctl enable api
