729ea4a21259f0ca08a4f0ef1e21e9d1a7b8585d
Brendan Leonard <brendan_leonard@hks.harvard.edu>
2020-07-09 10:26:11 -0400

    Add keywords endpoint

--- country_tools_api/schemas/hub.py
+++ country_tools_api/schemas/hub.py
@@ -1,6 +1,8 @@
 import graphene
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import func
+from collections import Counter
 
 from database.base import db_session
 from database import hub as hub_db
@@ -14,6 +16,11 @@ class HubProjects(SQLAlchemyObjectType):
         interfaces = (graphene.relay.Node,)
 
 
+class HubKeywords(graphene.ObjectType):
+    keyword = graphene.String()
+    projects = graphene.Int()
+
+
 class HubQuery(graphene.ObjectType):
     """Hub query objects for GraphQL API."""
 
@@ -21,6 +28,7 @@ class HubQuery(graphene.ObjectType):
     hub_projects = graphene.Field(
         HubProjects, project_name=graphene.String(required=True)
     )
+    hub_keywords_list = graphene.List(HubKeywords)
 
     def resolve_hub_projects_list(self, info, **args):
         return db_session.query(hub_db.HubProjects)
@@ -31,3 +39,21 @@ class HubQuery(graphene.ObjectType):
             .filter(getattr(hub_db.HubProjects, "project_name") == args["project_name"])
             .one()
         )
+
+    def resolve_hub_keywords_list(self, info, **args):
+        keywords = []
+
+        c = Counter(
+            [
+                x[0]
+                for x in (
+                    db_session.query(func.unnest(hub_db.HubProjects.keywords)).all()
+                )
+            ]
+        )
+
+        for key, count in c.items():
+            keywords.append(HubKeywords(key, count))
+
+        return keywords
+
